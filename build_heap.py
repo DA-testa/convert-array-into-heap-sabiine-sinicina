96bb3f653ccdb1871cfbd27effcb568ac0981c9a
sabiine-sinicina <124919082+sabiine-sinicina@users.noreply.github.com>
2023-03-12 19:57:48 +0000

    test5

--- build_heap.py
+++ build_heap.py
@@ -3,23 +3,46 @@
 
 def build_heap(data):
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
+    n = len(data)
 
+    # Build the heap bottom-up by sifting down each parent node
+    for i in range((n-1)//2, -1, -1):
+        parent = i
+        while parent*2+1 < n:
+            left_child = parent*2+1
+            right_child = parent*2+2 if parent*2+2 < n else left_child
+            min_child = left_child if data[left_child] < data[right_child] else right_child
+            if data[parent] > data[min_child]:
+                swaps.append((parent, min_child))
+                data[parent], data[min_child] = data[min_child], data[parent]
+                parent = min_child
+            else:
+                break
 
     return swaps
 
 
 def main():
     
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
+    input_type = input()
 
+    if "F" in input_type:
+        filename = input()
+        if ".a" in filename:
+            return
+        if "test/" not in filename:
+            filename = "test/" + filename
+        if "test/" in filename:    
+            with open(filename) as f:
+                n = int(f.readline().strip())
+                data = list(map(int, f.readline().strip().split()))
+    elif "I" in input_type:
+        n = int(input())
+        data = list(map(int, input().split()))
+        height = compute_height(n, parents)
+    else:
+        return()    
 
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
